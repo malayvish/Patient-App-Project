1e30e3179bff031059f6847b849eead21d7c0ad4
Malay Vishwakarma <144776233+malayvish@users.noreply.github.com>
2025-09-19 00:52:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from reportlab.lib.styles import getSampleStyleSheet
 from io import BytesIO
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
@@ -1109,4 +1109,5 @@ class PatientManagementSystem(tk.Tk):
 
 if __name__ == "__main__":
     app = PatientManagementSystem()
+
     app.mainloop()
